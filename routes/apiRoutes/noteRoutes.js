c3f3011c9c1dacc0a91f7f47b48d930dc4196dcd
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-07-01 15:37:04 -0600

    got the json to add to the page

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { notes } = require('../../data/notes');
+const { notes } = require('../../data/notes.json');
+const path = require("path");
+const fs = require('fs');
 
 router.get('/notes', (req, res) => {
   let results = notes;
@@ -7,9 +9,20 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-  
+  let note = req.body;
 
-})
+  if (!note) {
+    res.status(400).send('Error');
+  } else {
+    notes.push(note);
+    fs.writeFile(
+      path.join(__dirname, '../../data/notes.json'),
+      JSON.stringify({notes}, null, 2)
+    );
+    res.json(notes);
+  }
+
+});
 
 
 module.exports = router;
