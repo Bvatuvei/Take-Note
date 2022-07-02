1f837892b070a012b93506fb9551cc836a3ed482
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-07-02 12:53:13 -0600

    updated uuid to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,24 @@
 const router = require('express').Router();
-const { notes } = require('../db/notes.json');
-const path = require("path");
-const fs = require('fs');
+const store = require('../db/store');
 
 router.get('/notes', (req, res) => {
-  let results = notes;
-  res.json(results);
+  store
+  .getNotes()
+  .then((notes) => {
+    return res.json(notes)
+  })
+  .catch((err) => res.status(500).json(err));
 });
 
 router.post('/notes', (req, res) => {
-  let note = req.body;
-
-  if (!note) {
-    res.status(400).send('Error');
-  } else {
-    notes.push(note);
-    fs.writeFileSync(
-      path.join(__dirname, '../../db/notes.json'),
-      JSON.stringify({notes}, null, 2)
-    );
-    res.json(notes);
-  }
+store
+.addNote(req.body)
+.then((note) => res.json(note))
+.catch((err) => res.status(500).json(err));
 
 });
+
+//router.delete()
 
 
 module.exports = router;
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const noteRoutes = require('./apiRoutes.js');
-
-router.use(noteRoutes);
-
-module.exports = router;
